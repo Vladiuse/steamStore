7927021df0c3d9a47da2ccf1146cc2df43c9c52f
vladiuse <vladiuse@gmail.com>
2024-07-12 17:45:11 +0300

    fix path of secrets

--- steam_store/steam_store/settings.py
+++ steam_store/steam_store/settings.py
@@ -14,17 +14,20 @@ from pathlib import Path
 import json
 import os
 
-def get_secrets():
-    with open('secrets.json') as file:
-        return json.load(file)
 
-
-SECRETS = get_secrets()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+def get_secrets():
+    file_name = 'secrets.json'
+    secrets_path = os.path.join(BASE_DIR, file_name)
+    with open(secrets_path) as file:
+        return json.load(file)
+
+
+SECRETS = get_secrets()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
